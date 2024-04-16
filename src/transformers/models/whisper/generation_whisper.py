df2d2d447bef1f32ed572a4d6ffc03c706f0fbcc
laurin <laurin@myreha.ai>
2024-04-16 07:26:03 +0000

    leave attentions untouched

--- src/transformers/models/whisper/generation_whisper.py
+++ src/transformers/models/whisper/generation_whisper.py
@@ -216,9 +216,9 @@ class WhisperGenerationMixin:
 
         if num_frames is None or isinstance(num_frames, int):
             # Normalize and smoothen the weights.
-            std = torch.std(weights, dim=-2, keepdim=True, unbiased=False)
-            mean = torch.mean(weights, dim=-2, keepdim=True)
-            weights = (weights - mean) / std
+            #std = torch.std(weights, dim=-2, keepdim=True, unbiased=False)
+            #mean = torch.mean(weights, dim=-2, keepdim=True)
+            #weights = (weights - mean) / std
             weights = _median_filter(weights, self.config.median_filter_width)
 
             # Average the different cross-attention heads.
@@ -238,9 +238,9 @@ class WhisperGenerationMixin:
                 matrix = weights[batch_idx, :, non_special_tokens_indices, :num_frames[batch_idx] // 2]
 
                 # Normalize and smoothen the weights.
-                std = torch.std(matrix, dim=-2, keepdim=True, unbiased=False)
-                mean = torch.mean(matrix, dim=-2, keepdim=True)
-                matrix = (matrix - mean) / std
+                #std = torch.std(matrix, dim=-2, keepdim=True, unbiased=False)
+                #mean = torch.mean(matrix, dim=-2, keepdim=True)
+                #matrix = (matrix - mean) / std
                 matrix = _median_filter(matrix, self.config.median_filter_width)
 
                 # Average the different cross-attention heads.
